446e8f110d8530ce974ebba9472b85e07f2c4811
AndriiKosanyak <andriikosanyak4@gmail.com>
2020-12-10 21:26:35 +0200

    upd

--- migration/src/configs/sequelize.config.js
+++ migration/src/configs/sequelize.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": 'root',
+    "password": '123hello',
     "database": "users_cars",
     "host": "127.0.0.1",
     "dialect": "mysql"
