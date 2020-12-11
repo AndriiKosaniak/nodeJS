1d7e904c0782043597ee883978ff6da5f5a3023d
AndriiKosanyak <andriikosanyak4@gmail.com>
2020-12-11 17:59:13 +0200

    fixed

--- migration/src/app.js
+++ migration/src/app.js
@@ -13,6 +13,13 @@ app.use(express.urlencoded({extended: true}));
 app.use('/users', usersRouter);
 app.use('/register', registrationRouter);
 
+app.use('*', (error, req, res, next) => {
+    res.status(error.code).json({
+        message: error.message,
+        ok: false
+    })
+});
+
 
 sequelize.sync({alter: true})
     .then(() => app.listen(5000, err => err && console.log(err) || console.log(`Listen ${port}`)))
